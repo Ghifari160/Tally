3e1f57ea10b191881cfe34f9e9aa9427d7e4e820
Ghifari160 <ghifari160@ghifari160.com>
2018-05-16 00:15:53 -0500

    Add main UI
    Legacy Tally was a one-page app. Future versions of Tally will, for the
    most part, continue that. The only exceptions are the pages unrelated
    to the functions of the app (i.e. the about page and the bug report
    generator).

---
+++ tally.php
@@ -0,0 +1,43 @@
+<?php require_once "app.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Tally.Dev</title>
+<?php
+tally_meta();
+recall_styles();
+recall_scripts();
+?>
+</head>
+
+<body>
+<header class="app-header">
+<?php tally_header(); ?>
+</header>
+
+<div class="app-body">
+  <div class="tally-input">
+    <input class="item-input" placeholder="Enter an item, press enter/return..."
+        autocomplete="off" autocorrect="off" type="text">
+  </div>
+
+  <div class="tally-list">
+    <div class="ui top">
+      <div>Click on each item to decrease its count</div>
+      <div>
+        <ul>
+          <li><a href="#" class="options">Options</a></li>
+          <li><a href="#" class="export">Export</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="list"></div>
+  </div>
+</div>
+
+<div class="app-footer">
+<?php tally_footer(); ?>
+</div>
+</body>
+</html>
