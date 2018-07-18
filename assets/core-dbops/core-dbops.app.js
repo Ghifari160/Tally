33e2f6a5066cb9abac245dbb8e3fa196a77c9779
Ghifari160 <ghifari160@ghifari160.com>
2018-07-17 19:08:55 -0500

    Add core-dbops scripts
    Temporary scripts to enable core-dbops UX improvements.
    
    - Add core-dbops/core-dbops.app.js
    - Add core-dbops/core-dbops.js

---
+++ assets/core-dbops/core-dbops.app.js
@@ -0,0 +1,11 @@
+(function( $ )
+{
+  $(document).ready(function()
+  {
+    window._tally.tally_enqueue_menu_options("com.ghifari160.tally.dbops.store",
+        "checkbox", "", "dbops-store", function(p)
+        {
+          // TODO
+        }, "");
+  });
+}) ( jQuery );
